8ea4929cb2074b53d6b0be57a18390925ca37d2c
blake <beblancett@gmail.com>
2021-10-01 23:22:34 -0400

    Added barebones templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Blake'}
+    posts = [
+        {
+            'author': {'username': 'Charlie'},
+            'body': 'Beautiful day in Dayton!'
+        },
+        {
+            'author': {'username': 'Claire'},
+            'body': 'Ted Lasso is so good!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
